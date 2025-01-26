a4f2f5b65d6004da6b2cbc7e0089b13deabd47e4
Kallistraw <195604760+Kallistraw@users.noreply.github.com>
2025-01-26 13:09:12 +0700

    Create chatbot.py

---
+++ modules/chatbot.py
@@ -0,0 +1,17 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
+import json
+
+chatbot = ChatBot(
+    'Kc0',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    database_uri='sqlite:///database.sqlite3'  # Database for storing data
+)
+
+with open('dataset.json', 'r') as f:
+    data = json.load(f)
+
+trainer = ListTrainer(chatbot)
+
+for pair in data:
+    trainer.train(pair)
